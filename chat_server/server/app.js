eab814881a66a01c1ded3a9dfc108298a8df48d9
LC4311 <16984757+LC4311@users.noreply.github.com>
2019-07-05 17:59:14 +0800

    added chat server

---
+++ chat_server/server/app.js
@@ -0,0 +1,21 @@
+
+var express = require('express');
+var app = express();
+var socket = require('socket.io');
+
+
+server = app.listen(8080, function(){
+    console.log('server is running on port 8080')
+});
+
+io = socket(server);
+
+io.on('connection', (socket) => {
+    // console.log(socket)
+    socket.on('SEND_MESSAGE', function(data){
+        console.log(data,".")
+        console.log(socket.id)
+        io.emit('RECEIVE_MESSAGE', data);
+    })
+});
+
