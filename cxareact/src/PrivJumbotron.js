af04b65257455f05cb18bb44a8997f15d73ab600
A Sentient Being <ericaong@users.noreply.github.com>
2019-07-05 13:46:48 +0800

    chat
    Added Chat

--- cxareact/src/PrivJumbotron.js
+++
@@ -1,8 +0,0 @@
-import React, {useState, useEffect} from 'react';
-import { Navbar, Nav, NavDropdown, Form, Button, FormControl } from 'react-bootstrap';
-import Sidebar from './Sidebar.js'
-import './Navbar.css';
-
-let 
-
-export default App;
